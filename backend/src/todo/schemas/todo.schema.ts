a17a4072081bac0e912b29b82b430ac39faa86ed
Andrii Solohub <andrei.sologub12@gmail.com>
2022-08-19 16:07:18 +0300

    fixed lint issues

--- backend/src/todo/schemas/todo.schema.ts
+++ backend/src/todo/schemas/todo.schema.ts
@@ -5,17 +5,17 @@ export type TodoDocument = Todo & Document<string>;
 
 @Schema()
 export class Todo {
-    @Prop()
-    description: string;
+  @Prop()
+  description: string;
 
-    @Prop({ default: false })
-    done: boolean;
+  @Prop({ default: false })
+  done: boolean;
 
-    @Prop({ default: new Date() })
-    created_at: Date
+  @Prop({ default: new Date() })
+  created_at: Date;
 
-    @Prop({ default: new Date() })
-    updated_at: Date
+  @Prop({ default: new Date() })
+  updated_at: Date;
 }
 
 export const TodoSchema = SchemaFactory.createForClass(Todo);
