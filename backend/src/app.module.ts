4254de6d3ec5cc6dd60a366e0f79fec63106d5f2
Andrii Solohub <andrei.sologub12@gmail.com>
2022-08-22 17:37:24 +0300

    uri instead hardcode

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,20 @@
 import { Module } from '@nestjs/common';
 import { TodoModule } from './todo/todo.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { config } from 'dotenv';
+import { join } from 'path';
+
+config();
+const uri = process.env.MONGODB_URI;
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '../../frontend/dist'),
+    }),
     MongooseModule.forRoot(
-      '',
+      uri as string,
     ),
     TodoModule,
   ],
