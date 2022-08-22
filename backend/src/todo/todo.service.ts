99df5f2a9733cf7024ed6329cef630f2b4fec45c
Andrii Solohub <andrei.sologub12@gmail.com>
2022-08-22 20:50:06 +0300

    fixed linter issues

--- backend/src/todo/todo.service.ts
+++ backend/src/todo/todo.service.ts
@@ -22,9 +22,9 @@ export class TodoService {
 
   async update(id: string, updateTodoDto: UpdateTodoDto): Promise<Todo> {
     const existingTodo = await this.todoModel.findById(id);
-    if (!existingTodo){
-			throw new HttpException('Not Found', HttpStatus.NOT_FOUND)
-		}
+    if (!existingTodo) {
+      throw new HttpException('Not Found', HttpStatus.NOT_FOUND);
+    }
     existingTodo.description =
       updateTodoDto.description ?? existingTodo.description;
     existingTodo.done = updateTodoDto.done ?? existingTodo.done;
