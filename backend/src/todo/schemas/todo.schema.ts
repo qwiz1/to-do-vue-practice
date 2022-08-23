b8a161b01c344dd4dfe59fd6a6946e4c130776be
Andrii Solohub <andrei.sologub12@gmail.com>
2022-08-23 12:37:25 +0300

    edited schema - added option timestamps instead defined in props

--- backend/src/todo/schemas/todo.schema.ts
+++ backend/src/todo/schemas/todo.schema.ts
@@ -3,19 +3,15 @@ import { Document } from 'mongoose';
 
 export type TodoDocument = Todo & Document<string>;
 
-@Schema()
+@Schema({
+  timestamps: true,
+})
 export class Todo {
   @Prop()
   description: string;
 
   @Prop({ default: false })
   done: boolean;
-
-  @Prop({ default: new Date() })
-  created_at: Date;
-
-  @Prop({ default: new Date() })
-  updated_at: Date;
 }
 
 export const TodoSchema = SchemaFactory.createForClass(Todo);
